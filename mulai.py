cc5f03301fcef4f9d8cc386c467d1a24cb52fc72
bimawae1 <bimantara.dev@gmail.com>
2025-03-10 15:19:39 +0700

    Update mulai.py

--- mulai.py
+++ mulai.py
@@ -26,6 +26,6 @@ app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-if name == "main":
+if __name__ == __"main"__:
     print("Bot berjalan...")
     app.run_polling()
